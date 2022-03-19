cf34f62c07ee1c4754d314d5ddde08611c5c2e2c
Guy Marshall <guymarshall9@gmail.com>
2022-03-19 11:43:56 +0000

    Fix scripts and styles not applying

--- pages/page.php
+++ pages/page.php
@@ -8,10 +8,10 @@ class Page {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="../styles/styles.css">
+        <link rel="stylesheet" href="/styles/styles.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-        <script src="../scripts/todo.js"></script>';
+        <script src="/scripts/todo.js"></script>';
 
         $body_html = '
         <header>
@@ -24,13 +24,13 @@ class Page {
                     <div class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
                         <ul class="navbar-nav flex-grow-1">
                             <li class="nav-item">
-                                <a class="nav-link text-dark" href="./index.php">Home</a>
+                                <a class="nav-link text-dark" href="/pages/index.php">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-dark" href="./todo.php">Todo</a>
+                                <a class="nav-link text-dark" href="/pages/todo.php">Todo</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-dark" href="./collatz.php">Collatz</a>
+                                <a class="nav-link text-dark" href="/pages/collatz.php">Collatz</a>
                             </li>
                         </ul>
                     </div>
